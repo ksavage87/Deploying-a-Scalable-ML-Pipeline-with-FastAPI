d3e956057766c52b54c32aa46222a03bdf7ab5f1
kaleysavage <kmaye26@wgu.edu>
2024-06-10 08:24:41 -0500

    Retrying model

--- test_ml.py
+++ test_ml.py
@@ -1,22 +1,26 @@
-# TODO: add necessary import
+import os
 import pytest
 import numpy as np
 import pickle
 from sklearn.ensemble import RandomForestClassifier
-import sys
-sys.path.append('Users/kaleymayer/Deploying-a-Scalable-ML-Pipeline-with-FastAPI')
-from train_model import X_test, y_test, compute_metrics
 from sklearn.metrics import fbeta_score, precision_score, recall_score, accuracy_score
 
+# Get the absolute path to the directory containing this script
+project_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Construct the path to the model file relative to the project directory
+model_path = os.path.join(project_dir, "model", "model.pkl")
+print(f"Loading model from {model_path}")
 
 # Load the model from model.pkl
-model_path = "/Users/kaleymayer/Deploying-a-Scalable-ML-Pipeline-with-FastAPI/model/model.pkl"
-print(f"Loading model from {model_path}")
 with open(model_path, "rb") as f:
     model = pickle.load(f)
 
+# Load necessary data from train_model.py
+sys.path.append(os.path.join(project_dir, "train_model.py"))
+from train_model import X_test, y_test, compute_metrics
 
-# TODO: implement the first test. Change the function name and input as needed
+# Implement the first test
 def test_ml_function_type():
     """
     # Tests if the ML function returns the expected type of result.
@@ -24,20 +28,15 @@ def test_ml_function_type():
     prediction = model.predict(X_test)
     prediction_array = list(prediction)
     assert isinstance(prediction_array, list)
-    pass
 
-
-# TODO: implement the second test. Change the function name and input as needed
+# Implement the second test
 def test_ml_algorithm():
     """
     # Tests if the ML model uses the expected algorithm.
     """
     assert isinstance(model, RandomForestClassifier)
-    pass
 
-
-
-# TODO: implement the third test. Change the function name and input as needed
+# Implement the third test
 def test_compute_metrics():
     """
     # Tests if the computing metrics function returns the expected values.
@@ -45,4 +44,3 @@ def test_compute_metrics():
     prediction = model.predict(X_test)
     metrics = compute_metrics(y_test, prediction)
     assert isinstance(metrics, dict)
-    pass
