a2050c67e89874ea05446aca8cf059ccf385755f
kaleysavage <kmaye26@wgu.edu>
2024-06-11 08:15:48 -0500

    More flake8 changes

--- ml/model.py
+++ ml/model.py
@@ -85,12 +85,12 @@ def performance_on_categorical_slice(data,
     sliced_data = data[data[column_name] == slicevalue]
 
     X_slice, y_slice, _, _ = process_data(
-    sliced_data,
-    categorical_features,
-    label,
-    training=False,
-    encoder=encoder,
-    lb=lb
+        sliced_data,
+        categorical_features,
+        label,
+        training=False,
+        encoder=encoder,
+        lb=lb
 )
 
     preds = inference(model, X_slice)
--- test_ml.py
+++ test_ml.py
@@ -1,48 +1,27 @@
-import os
 import pickle
 import pytest
 from sklearn.ensemble import RandomForestClassifier
 from train_model import X_test, y_test, compute_metrics
 
-# Get the absolute path to the directory containing this script
-project_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Construct the path to the model file relative to the project directory
-model_path = os.path.join(project_dir, "model", "model.pkl")
+model_path = "model/model.pkl"
 print(f"Loading model from {model_path}")
 
 # Load the model from model.pkl
 with open(model_path, "rb") as f:
     model = pickle.load(f)
 
-# Test if the ML function returns the expected type of result
-def test_prediction_type():
 
-    
-    """
-    Tests if the ML function returns the prediction as a list.
-    """
+def test_prediction_type():
     prediction = model.predict(X_test)
     assert isinstance(prediction, list)
 
-    
-    
-    
-# Test if the ML model uses the expected algorithm
+
 def test_model_algorithm():
-    """
-    Tests if the ML model is an instance of RandomForestClassifier.
-    """
     assert isinstance(model, RandomForestClassifier)
 
-    
-    
-    
-# Test if the compute_metrics function returns the expected values
+
 def test_compute_metrics():
-    """
-    Tests if the compute_metrics function returns a dictionary.
-    """
     prediction = model.predict(X_test)
     metrics = compute_metrics(y_test, prediction)
     assert isinstance(metrics, dict)
@@ -50,10 +29,7 @@ def test_compute_metrics():
     assert "precision" in metrics
     assert "recall" in metrics
 
-    
+
 if __name__ == "__main__":
-    
-    
     pytest.main()
-    
     
